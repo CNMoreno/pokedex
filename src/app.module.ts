2e5092818832c7037bc9371715030894f2ed6303
Cristian Moreno <mateomoreno123444@gmail.com>
2022-12-14 18:00:24 -0500

    connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+    rootPath: join(__dirname,'..','public'),
+    }), 
+    MongooseModule.forRoot('mongodb://localhost:27017/pokemon'),
+    PokemonModule
+  ],
 })
 export class AppModule {}
