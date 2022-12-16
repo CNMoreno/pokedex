bf18d762637a524c1bd5567b670b0cfcf29bc2a0
Cristian Moreno <mateomoreno123444@gmail.com>
2022-12-16 18:32:52 -0500

    add logic on service pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 
 @Module({
@@ -11,7 +12,8 @@ import { PokemonModule } from './pokemon/pokemon.module';
     rootPath: join(__dirname,'..','public'),
     }), 
     MongooseModule.forRoot('mongodb://localhost:27017/pokemon'),
-    PokemonModule
+    PokemonModule,
+    CommonModule
   ],
 })
 export class AppModule {}
